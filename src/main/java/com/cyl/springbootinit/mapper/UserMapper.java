0fd7ffc92d62d5f384737b2f5534132736d6944b
YoungLin <2098997424@qq.com>
2024-06-02 20:24:04 +0800

    init commit

---
+++ src/main/java/com/cyl/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.cyl.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cyl.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/cmaiyatang">YoungLin</a>
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
